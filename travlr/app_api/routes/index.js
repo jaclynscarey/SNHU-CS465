f3b4bb04b1e9fe8b96d0973a9b9185d169ae8216
Jaclyn Carey <jaclyn.carey@snhu.edu>
2025-04-13 22:31:16 -1000

    Module 6 completed baseline

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -7,10 +7,13 @@ const tripsController = require('../controllers/trips');
 // Define route for our trips endpoint
 router
     .route("/trips")
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList) // GET Method routes tripsList
+    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
 
 // GET Method routes tripsFindByCode - requires parameter
+// PUT Method routes tripsUpdateTrip - requires parameter
 router
     .route("/trips/:tripCode")
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 module.exports = router;
